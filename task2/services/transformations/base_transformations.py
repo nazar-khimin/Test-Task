6e14e2853ba62f52663c1a10f1fe59464f2e7be0
nazar.khimin <nazar.khimin@unanet.com>
2025-04-13 02:33:23 +0300

    removed unnecessary transformation

--- task2/services/transformations/base_transformations.py
+++ task2/services/transformations/base_transformations.py
@@ -16,20 +16,3 @@ def trim_all_values(df: pd.DataFrame, schema: dict[str, str]) -> pd.DataFrame:
             # Convert all values to strings and trim spaces
             df[column] = df[column].astype(str).str.strip()
     return df
-
-
-@timeit
-def convert_data_types(df: pd.DataFrame, schema: dict[str, str]) -> pd.DataFrame:
-    """
-    Converts the DataFrame columns to the desired types based on the schema:
-    """
-    for column, target_type in schema.items():
-        if column not in df.columns:
-            print(f"Column '{column}' is not in the DataFrame; skipping conversion.")
-            continue
-
-        if target_type == "date":
-            df[column] = pd.to_datetime(df[column], errors="coerce")
-        elif target_type == "int":
-            df[column] = pd.to_numeric(df[column], errors="coerce").astype("Int64")
-    return df
