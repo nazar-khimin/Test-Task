0106a1bfe7cf9bc9b96000c2ad7158034260469b
nazar.khimin <nazar.khimin@unanet.com>
2025-04-13 02:30:07 +0300

    added separate data inserter and fixed converter

--- task2/services/transformations/employees_transformations.py
+++ task2/services/transformations/employees_transformations.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-from task2.services.transformations.base_transformations import trim_string_values, convert_data_types
+from task2.services.transformations.base_transformations import trim_all_values, convert_data_types
 
 
 def transform_csv_data(df: pd.DataFrame, schema: dict[str, str]) -> pd.DataFrame:
     """
     First trim string columns, then convert data types based on the provided schema.
     """
-    df = trim_string_values(df, schema)
+    df = trim_all_values(df, schema)
     df = convert_data_types(df, schema)
     return df
