452c7c416d29e1fd4c802cff51a9157afdd2df41
nazar.khimin <nazar.khimin@unanet.com>
2025-04-12 22:36:34 +0300

    added dependecies, sample csv file correspond headers and base sql lite model

---
+++ task2/models/base_model.py
@@ -0,0 +1,15 @@
+import uuid
+
+from sqlalchemy import create_engine, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+    id: Mapped[str] = mapped_column(
+        String, primary_key=True, default=lambda: str(uuid.uuid4())
+    )
+
+DATABASE_URL = "sqlite:///company.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine)
