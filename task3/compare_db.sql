1a8f0dfc3a0776c7003822c226a9ad0363f861d6
nazar.khimin <nazar.khimin@unanet.com>
2025-04-13 18:21:13 +0300

    added compare db query which find differences in values and missed records

--- task3/compare_db.sql
+++ task3/compare_db.sql
@@ -1,39 +1,34 @@
-ATTACH DATABASE 'task2/db/company1.db' AS cp1;
-ATTACH DATABASE 'task3/db/company2.db' AS cp2;
+ATTACH DATABASE 'task2/db/company1.db' AS c1;
+ATTACH DATABASE 'task3/db/company2.db' AS c2;
 
+-- Count rows in both tables
+-- SELECT
+--     (SELECT COUNT(*) FROM c1.employees) AS company1_count,
+--     (SELECT COUNT(*) FROM c2.employees) AS company2_count;
+
+--- Compare from company1 to company2
 SELECT
-    id,
-    name,
-    date_of_birth,
-    salary,
-    department_id
-FROM cp1.employees
-EXCEPT
-SELECT
-    emp_id,
-    full_name,
-    dob,
-    salary,
-    department_id
-FROM cp2.employees
+    c1e.*,
+    c2e.*
+FROM c1.employees c1e
+LEFT JOIN c2.employees c2e ON c1e.name = c2e.full_name
+WHERE c2e.full_name IS NULL
+   OR c1e.date_of_birth != c2e.dob
+   OR c1e.salary != c2e.salary
+   OR c1e.department_id != c2e.department_id
 
 UNION ALL
 
+-- Compare from company2 to company1
 SELECT
-    emp_id,
-    full_name,
-    dob,
-    salary,
-    department_id
-FROM cp2.employees
-EXCEPT
-SELECT
-    id,
-    name,
-    date_of_birth,
-    salary,
-    department_id
-FROM cp1.employees;
+    c1e.*,
+    c2e.*
+FROM c2.employees c2e
+LEFT JOIN c1.employees c1e ON c2e.full_name = c1e.name
+WHERE c1e.name IS NULL
+   OR c1e.date_of_birth != c2e.dob
+   OR c1e.salary != c2e.salary
+   OR c1e.department_id != c2e.department_id;
 
-DETACH DATABASE cp1;
-DETACH DATABASE cp2;
+DETACH DATABASE c1;
+DETACH DATABASE c2;
--- task3/table_info.sql
+++
@@ -1,8 +0,0 @@
-ATTACH DATABASE 'task2/db/company1.db' AS cp1;
-ATTACH DATABASE 'task3/company2.db' AS cp2;
-
--- PRAGMA cp1.table_info('employees');
-PRAGMA cp2.table_info('employees');
-
-DETACH DATABASE cp1;
-DETACH DATABASE cp2;
