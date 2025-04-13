c0f836cf63d14f5128b8e0a11e42dfc2dacf0ad3
nazar.khimin <nazar.khimin@unanet.com>
2025-04-13 18:31:22 +0300

    added joins to department

--- task3/compare_db.sql
+++ task3/compare_db.sql
@@ -9,9 +9,13 @@ ATTACH DATABASE 'task3/db/company2.db' AS c2;
 --- Compare from company1 to company2
 SELECT
     c1e.*,
-    c2e.*
+    c1d.name AS c1_dept_name,
+    c2e.*,
+    c2d.dept_name AS c2_dept_name
 FROM c1.employees c1e
 LEFT JOIN c2.employees c2e ON c1e.name = c2e.full_name
+LEFT JOIN c1.departments c1d ON c1e.department_id = c1d.id
+LEFT JOIN c2.departments c2d ON c2e.department_id = c2d.dept_id
 WHERE c2e.full_name IS NULL
    OR c1e.date_of_birth != c2e.dob
    OR c1e.salary != c2e.salary
@@ -22,9 +26,13 @@ UNION ALL
 -- Compare from company2 to company1
 SELECT
     c1e.*,
-    c2e.*
+    c1d.name AS c1_dept_name,
+    c2e.*,
+    c2d.dept_name AS c2_dept_name
 FROM c2.employees c2e
 LEFT JOIN c1.employees c1e ON c2e.full_name = c1e.name
+LEFT JOIN c1.departments c1d ON c1e.department_id = c1d.id
+LEFT JOIN c2.departments c2d ON c2e.department_id = c2d.dept_id
 WHERE c1e.name IS NULL
    OR c1e.date_of_birth != c2e.dob
    OR c1e.salary != c2e.salary
